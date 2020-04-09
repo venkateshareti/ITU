ea636512fe27b476d24c74459bb2e837f73c1ddd
Sailaja Gudala <sailaja.gudala@equinox.com>
2020-04-09 21:07:06 +0530

    script change

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -1,16 +1,16 @@
 #stop apache server
 systemctl stop httpd
 # removing the existing code
-rm -rf /opt/ITU_ENV1
+rm -rf /opt/ITU_ENV
 # created new environment
-mkdir /opt/ITU_ENV1
+mkdir /opt/ITU_ENV
 # creating virtual environment
 python3 -m virtualenv /opt/ituvenv
-cd /opt/ITU_ENV1
+cd /opt/ITU_ENV
 cp -r /opt/latest_code/ITU .
 source /opt/ituvenv/bin/activate
 echo "virtual environment activated"
-cd /opt/ITU_ENV1/ITU
+cd /opt/ITU_ENV/ITU
 pip install -r requirements.txt
 
 systemctl start httpd
