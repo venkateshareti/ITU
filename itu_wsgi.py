f1df5f7e1cc39e50b99f743a2c6dbf129bef2c6b
Sailaja Gudala <sailaja.gudala@equinox.com>
2020-04-16 21:53:19 +0530

    added ITU config files

---
+++ itu_wsgi.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3.6
+
+import os, sys
+
+python_home = '/opt/ituflask/ituvenv'
+from unsmin.config import basedir
+sys.path.insert(0, '/opt/ituflask/ITU_ENV/ITU')
+
+#activate_this = '/opt/ituflask/ituvenv/bin/activate_this.py'
+#with open(activate_this) as file_:
+#    exec(file_.read(), dict(__file__=activate_this))
+
+config_name = os.environ['FLASK_ENV']
+
+from unsmin.app import create_app
+
+application = create_app(config_name)
